7b33aaeea88f69254186bc51730ec5320078ee27
elboy <elboy@stanford.edu>
2015-08-04 13:54:37 -0400

    Publish to admin

--- server/publications.js
+++ server/publications.js
@@ -1,17 +1,28 @@
 // publishes data of current order, from current user
-Meteor.publish('order', function(currentOrder){
+Meteor.publish('currentOrder', function(currentOrder){
 	var userId = this.userId;
 	return Orders.find({user_id: userId, _id: currentOrder});
 });
 
 //** CAUTION ** contains all user orders
-Meteor.publish('orders', function(){
+Meteor.publish('userOrders', function(){
 	var userId = this.userId;
 	return Orders.find({user_id: userId});
 });
 
+//** CAUTION ** contains all orders
+Meteor.publish('allOrders', function(){
+	return Orders.find({});
+});
+
 // publishes data of pictures corresponding to current order
-Meteor.publish('photos', function(orderId){
+Meteor.publish('userPhotos', function(orderId){
 	var userId = this.userId;
 	return Images.find({user_id: userId, order_id: orderId});
+});
+
+// publishes data of pictures corresponding to current order
+Meteor.publish('allPhotos', function(orderId){
+	var userId = this.userId;
+	return Images.find({});
 });
